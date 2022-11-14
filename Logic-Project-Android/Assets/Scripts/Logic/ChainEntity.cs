81ae6373ee2126dad88b9427720b83cd9a5096fe
Doomkradchik <nekitamk26@gmail.com>
2022-11-14 16:41:08 +0200

    Test node

---
+++ Logic-Project-Android/Assets/Scripts/Logic/ChainEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class ChainEntity
+{
+    public ChainEntity(ChainEntity[] neighbours, bool state)
+    {
+        Neighbours = neighbours;
+        _isActive = state;
+    }
+    public event Action StateChanged;
+    public ChainEntity[] Neighbours { get; }
+    private bool _isActive;
+    public bool isActive
+    {
+        get => _isActive;
+        private set
+        {
+            _isActive = value;
+            foreach (var neightbour in Neighbours)
+                neightbour.ChangeState();
+        }
+    }
+    public void OnStart()
+    {
+        Validate(this);
+        StateChanged?.Invoke();
+    }
+    public void ChangeState()
+    {
+        isActive = !isActive;
+        StateChanged?.Invoke();
+    }
+    public void Validate(ChainEntity parent)
+    {
+        foreach (var entity in Neighbours)
+        {
+            if (entity.Equals(parent))
+                throw new InvalidOperationException("Circular dependency detected");
+
+            entity.Validate(parent);
+        }
+    }
+}
---
+++ Logic-Project-Android/Assets/Scripts/View/Node.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [SerializeField]
+    private bool _isActive;
+
+    [SerializeField]
+    private Node[] _nodes;
+
+    public ChainEntity ChainEntity { get; private set; }
+    private void Awake()
+    {
+        ChainEntity = new ChainEntity(
+            _nodes.Select(node => node.ChainEntity).ToArray(), _isActive);
+
+        ChainEntity.StateChanged += () => GetComponent<SpriteRenderer>().color =
+        ChainEntity.isActive ? Color.green : Color.red;
+    }
+
+    private void Start()
+    {
+        ChainEntity.OnStart();
+    }
+
+    private void OnMouseDown()
+    {
+        ChainEntity.ChangeState();
+    }
+}
